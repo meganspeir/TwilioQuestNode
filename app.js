ce106fa345748367b7059208bb14c27486e2979e
Megan Speir <no-reply@meganspeir.com>
2013-09-23 20:37:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+// Load app dependencies
+var http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    twilio = require('twilio');
+
+// Load configuration information from system environment variables.
+var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+
+// Create an authenticated client to access the Twilio REST API
+var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+// Create an Express web application with some basic configuration
+var app = express();
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
+
+// render our home page
+app.get('/', function(request, response) {
+    response.render('index');
+});
+
+// handle a POST request to send a text message.  This is sent via ajax on our
+// home page
+app.post('/message', function(request, response) {
+    // Use the REST client to send a text message
+    client.sendSms({
+        to: request.param('to'),
+        from: TWILIO_NUMBER,
+        body: 'Good luck on your Twilio quest!'
+    }, function(err, data) {
+        // When we get a response from Twilio, respond to the HTTP POST request
+        response.send('Message is inbound!');
+    });
+});
+
+// handle a POST request to make an outbound call.  This is sent via ajax on our
+// home page
+app.post('/call', function(request, response) {
+    // Use the REST client to send a text message
+    client.makeCall({
+        to: request.param('to'),
+        from: TWILIO_NUMBER,
+        url: 'http://twimlets.com/message?Message%5B0%5D=http://demo.kevinwhinnery.com/audio/zelda.mp3'
+    }, function(err, data) {
+        // When we get a response from Twilio, respond to the HTTP POST request
+        response.send('Call incoming!');
+    });
+});
+
+// Create a TwiML document to provide instructions for an outbound call
+app.get('/hello', function(request, response) {
+    // Create a TwiML generator
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('Hello there! You have successfully configured a web hook.');
+    twiml.say('Good luck on your Twilio quest!', {
+        voice:'woman'
+    });
+
+    // Return an XML response to this request
+    response.set('Content-Type','text/xml');
+    response.send(twiml.toString());
+});
+
+// Challenge 1:
+app.post('/incoming/call', function(request, response) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('You are learning node.js! Good for you.');
+    twiml.say('Welcome to your personal concierge service!')
+        .gather({
+            numDigits: 1,
+            action:'/handle-key'
+        }, function() {
+            this.say('Press 1 for customer service')
+                .say('Press 2 for British customer service', { language: 'en-gb'});
+        });
+
+    // Return an XML response to this request
+    response.set('Content-Type','text/xml');
+    response.send(twiml.toString());
+});
+
+app.get('/incoming/sms', function(request, response) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.sms('You can do node. Look at you go!');
+
+    response.set('Content-Type', 'text/xml');
+    response.send(twiml.toString());
+});
+
+app.post('/handle-key', function(request, response) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('Goodbye!', {language: 'en-gb'});
+});
+
+// Start our express app, by default on port 3000
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
